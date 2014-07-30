6e4e90b4193978cd3b2701a3b53d0a2618031019
Neel Palrecha <npalrecha@beatsmusic.com>
2014-07-30 16:37:40 -0700

    added mongodb to project

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 var Hapi = require('hapi');
+var dbOpts = {
+    "url": "mongodb://localhost:27017/snax",
+    "settings": {
+        "db": {
+            "native_parser": false
+        }
+    }
+};
+
 
 // Create a server with a host and port
 var server = new Hapi.Server('localhost', 8000);
+
+server.pack.register({
+    plugin: require('hapi-mongodb'),
+    options: dbOpts
+}, function (err) {
+    if (err) {
+        console.error(err);
+        throw err;
+    }
+});
 
 // public directory
 server.route({
