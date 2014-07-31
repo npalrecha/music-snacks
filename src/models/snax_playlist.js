aec9570a2ecb37d368500d403ef5190411e17c3f
Neel Palrecha <npalrecha@beatsmusic.com>
2014-07-30 17:30:58 -0700

    added snax playlist model

---
+++ src/models/snax_playlist.js
@@ -0,0 +1,13 @@
+var snax_playlist = function(args) {
+    args = args || {};
+
+    var self = {
+        artist: args.artist || '',
+        album: args.album || '',
+        title: args.title || '',
+        duration: args.duration
+    };
+};
+
+module.exports = snax_playlist;
+
