b5563c53f8a1aa5e28a78451c31a7d9244c5cc79
Neel Palrecha <npalrecha@beatsmusic.com>
2014-07-30 17:56:34 -0700

    Added track_ids to playlist_item model

--- src/models/playlist_item.js
+++ src/models/playlist_item.js
@@ -1,9 +1,16 @@
 var playlist_item = function(args) {
     args = args || {};
+    args.track_ids = args.track_ids || {};
 
     var self = {
         artist: args.artist || '',
         title: args.title || '',
+        track_ids: {
+            beats: args.track_ids.beats,
+            spotify: args.track_ids.spotify,
+            rdio: args.track_ids.rdio,
+            itunes: args.track_ids.itunes
+        },
         duration: args.duration
     };
 
